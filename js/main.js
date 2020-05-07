b22bfdc71d9c76529442ef52cdfdd4de02496eeb
acostade <acostade@gmail.com>
2020-05-07 11:50:35 -0500

    game structure

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*------Constants------*/
+// create panels colors as a const  
+//colors: res, yellow, blue, green, orange, purple. const variables 
+// create a button for each level
+
+
+
+/*------Variables------*/
+//level dificulty attached to level difficulty button
+//
+// array for storage sequence 
+
+
+
+/*------Functions------*/
+
+//init function 
+//random selection of button color.
+// create a sequence function  with the variable to storage panel. 
+//flash  function and add the time out for memorization. 
+//flash function for user select the panel. 
+
+
+// call back function that will let user to see if they click on the right panel of color else message game over. 
+
+
+//render function will group the game to start over 
+
+/*------Cached Element References------*/
+
+/*------Event Listeners------*/
