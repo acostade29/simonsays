803c5d710933cc341365543af3cf2021cbb7e5ed
acostade <acostade@gmail.com>
2020-05-12 23:27:59 -0500

    reorganizing javas code and adding the play button with logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-
-
-
-
 //cached elements
 // create the document element by id for enter name, start game, select color
 
@@ -9,51 +5,39 @@ const messageEl = document.getElementById("message");
 const messageInput = document.getElementById('inputMessage');
 const messageBtn = document.getElementById('messageButton');
 
-
-// turn count  and start  //
-const turnCounter = document.querySelector("#turnCounter");
-const startBtn = document.querySelector("#start")
-const strictButton = document.querySelector("#strict")
-const onButton = document.querySelector('#onText')
-
-//strict Button 
-// strictButton.addEventListener('click', (event) => {
-// if (strictButton.checked == true) {
-// strict = true;
-// } else { 
-// strict = false;
-// }
-// });
-
-//start button
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// defined message 
 //event handler
 // write a function that change the H1 TITLE to the name of the user once the included the name to Welcome + name
 messageBtn.addEventListener('click',function() {
     messageEl.innerText = `Welcome ${messageInput.value}!`;
 })
+
+
+// defined variables
+let strict = false;
+let noice = true;
+let on = false;
+let win;
+let turn;
+let compTurn;
+let intervalId;
+
+//create the  elements for each color 
+
+const blue = document.querySelector('blueButton');
+const red = document.querySelector('redButton');
+const yellow = document.querySelector('yellowButton');
+const green = document.querySelector('greenButton');
+const teal = document.querySelector('tealButton');
+const gray = document.querySelector('grayButton');
+const purple = document.querySelector('purpleButton');
+const orange = document.querySelector('orangeButton');
+
+// turn count  and start  //
+const turnCounter = document.querySelector("#turn");
+const startBtn = document.querySelector("#start")
+const strictButton = document.querySelector("#Strict")
+const onButton = document.querySelector('#on')
 
 
 //assign a color lookup to numbers//
@@ -68,44 +52,17 @@ let colorLookup ={
     8:'teal'
 }
 
-
-
 //create a sequence [1,5,4,6,2,3,7,8,1];
- let arry = [1,2,3,4,5,6,7,8,
+let arry = [0,1,2,3,4,5,6,7,8,
     1,2,3,4,5,6,7,8,
     1,2,3,4,5,6,7,8,
     1,2,3,4,5,6,7,8,
     1,2,3,4,5,6,7,8,
     1,2,3,4,5,6,7,8];
 
- //the sequence is not random we need to shuffle with a shuffle function 
- let arrayshuffle = function(arr) {
-    let newPos,
-    temp; 
-    for (let i = arr.length -1; i > 0; i--) {
-    newPos = Math.floor(Math.random() * (i + 1));   
-    temp = arr[i];
-    arr[i] = arr[newPos];
-    arr[newPos] = temp;
-    }
-    return arr;
- };
 
  
- var newSquence = arrayshuffle(arry);
 
-
-
-//create the  elements for each color 
-
-const blue = document.querySelector('blueButton');
-const red = document.querySelector('redButton');
-const yellow = document.querySelector('yellowButton');
-const green = document.querySelector('greenButton');
-const teal = document.querySelector('tealButton');
-const gray = document.querySelector('grayButton');
-const purple = document.querySelector('purpleButton');
-const orange = document.querySelector('orangeButton');
 
 
 //setting timer  and function flash  
@@ -125,66 +82,116 @@ function flash(){
 }    
 
 
-flash();
-function changeColor(color) {
-    if (color === 'Red') {
-        redButton.style.backgroundColor = 'white'
-    }
-    if (color === 'Yellow') {
-        yellowButton.style.backgroundColor = 'white'
-    }
-    if (color === 'Green') {
-        greenButton.style.backgroundColor = 'white'
-    }
-    if (color === 'Blue') {
-        blueButton.style.backgroundColor = 'white'
-    }
 
-    if (color === 'Gray') {
-        grayButton.style.backgroundColor = 'white'
+ //the sequence is not random we need to shuffle with a shuffle function 
+ let arrayshuffle = function(arr) {
+    let newPos,
+    temp; 
+    for (let i = arr.length -1; i > 0; i--) {
+    newPos = Math.floor(Math.random() * (i + 1));   
+    temp = arr[i];
+    arr[i] = arr[newPos];
+    arr[newPos] = temp;
     }
-    if (color === 'Purple') {
-        purpleButton.style.backgroundColor = 'white'
-    }
-    if (color === 'Teal') {
-        tealButton.style.backgroundColor = 'white'
-    }
-    if (color === 'Orange') {
-        orangeButton.style.backgroundColor = 'white'
-    }
-}
+    return arr;
+ };
 
-//create a fucntion to reset the color to original color 
-function colorReset() {
-    redButton.style.backgroundColor = 'red';
-    greenButton.style.backgroundColor = 'green';
-    yellowButton.style.backgroundColor = 'yellow';
-    blueButton.style.backgroundColor = 'blue';
-    grayButton.style.backgroundColor = 'gray';
-    purpleButton.style.backgroundColor = 'purple';
-    tealButton.style.backgroundColor = 'teal';
-    orangeButton.style.backgroundColor = 'orange';
-}
+ 
+ var newSquence = arrayshuffle(arry);
+//give the flash a background color of white 
 
-
-//create a render function that repeat everything. 
-
+ flash();
+ function changeColor(color) {
+     if (color === 'Red') {
+         redButton.style.backgroundColor = 'white'
+     }
+     if (color === 'Yellow') {
+         yellowButton.style.backgroundColor = 'white'
+     }
+     if (color === 'Green') {
+         greenButton.style.backgroundColor = 'white'
+     }
+     if (color === 'Blue') {
+         blueButton.style.backgroundColor = 'white'
+     }
+ 
+     if (color === 'Gray') {
+         grayButton.style.backgroundColor = 'white'
+     }
+     if (color === 'Purple') {
+         purpleButton.style.backgroundColor = 'white'
+     }
+     if (color === 'Teal') {
+         tealButton.style.backgroundColor = 'white'
+     }
+     if (color === 'Orange') {
+         orangeButton.style.backgroundColor = 'white'
+     }
+ }
+ 
+ //create a fucntion to reset the color to original color 
+ function colorReset() {
+     redButton.style.backgroundColor = 'red';
+     greenButton.style.backgroundColor = 'green';
+     yellowButton.style.backgroundColor = 'yellow';
+     blueButton.style.backgroundColor = 'blue';
+     grayButton.style.backgroundColor = 'gray';
+     purpleButton.style.backgroundColor = 'purple';
+     tealButton.style.backgroundColor = 'teal';
+     orangeButton.style.backgroundColor = 'orange';
+ }
  
 
 
 
 
-//start button work in progresss keep working !
-
-// const startButton = document.querySelector("#start")
-
-// const startBtn = document.getElementById('startBtn');
-
-// startButton.addEventListener('click',(event) => {
-//     flash()
-        
-// });
 
 
 
 
+
+
+
+
+// if the strict Button is check the strick funtion will be true or activated.  
+strictButton.addEventListener('click', (event) => {
+    if (strictButton.checked == true) {
+   strict = true;
+   } else { 
+   strict = false;
+   }
+   });
+   
+   //On Button with Score setting 
+   onButton.addEventListener('click' , (event) => {
+       if (onButton.checked == true) {
+           on = true;
+           turnCounter.innerHTML = "-";
+       } else {
+           on = false;
+           turnCounter.innerHTML = "--";
+           clearColor();
+           clearInterval(intervalId)
+       }
+   });
+   
+   //start button 
+   
+   startBtn.addEventListener('click',(event) => {
+    if (on || win) {
+    play();
+    }
+   })
+   
+   function play() {
+       win = false;
+       Order =[];
+       player = [];
+       flash = [0];
+       intervalId = 0;
+       turn = 1;
+       turnCounter.innerHTML = 1;
+       good =true;
+       
+   }
+   
